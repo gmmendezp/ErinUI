262112d3027d0440c3acc8c8d18cf29fb493549b
Cesar Redondo <me@redondocr.com>
2015-10-10 03:59:04 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name erinUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the erinUiApp
+ */
+angular.module('erinUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name erinUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the erinUiApp
+ */
+angular.module('erinUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
