c74e0f8ea94515699f8d6bda22d71aff7bec78bf
Jose Alavez <josecarlos215@gmail.com>
2015-10-11 04:02:05 -0600

    Adding imaging code

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,7 +7,7 @@
 
   function routeConfig($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/conflict', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
@@ -16,11 +16,6 @@
         templateUrl: 'app/login/login.html',
         controller: 'LoginController',
         controllerAs: 'login'
-      })
-      .when('/conflict-registration', {
-        templateUrl: 'app/conflict-registration/conflict-registration.html',
-        controller: 'ConflictRegistrationController',
-        controllerAs: 'conflict-registration'
       })
       .otherwise({
         redirectTo: '/'
